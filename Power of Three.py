012dd2792b7625ac5388e8b7cc1d5b0f1126ece6
graphMachine <wanglei430426@gmail.com>
2016-07-19 09:35:14 +0800

    Create Power of Three.py

---
+++ Power of Three.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n>0 and (math.log10(n)/math.log10(3)).is_integer()
