fb8458219d91c498808426462cc9c514aaaa572e
graphMachine <wanglei430426@gmail.com>
2016-07-20 17:57:25 +0800

    Create Product of Array Except Self.py

---
+++ Product of Array Except Self.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        record = [1 for i in range(len(nums))]
+        j=1
+        for i in range(1,len(nums)):
+            record[i]=j*nums[i-1]
+            j=j*nums[i-1]
+        j=1
+        for i in range(len(nums)-2,-1,-1):
+            record[i]=record[i]*j*nums[i+1]
+            j=j*nums[i+1]
+        return record
+            
+  """
+  Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Solve it without division and in O(n).
+
+For example, given [1,2,3,4], return [24,12,8,6].
+
+Follow up:
+Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+  """
