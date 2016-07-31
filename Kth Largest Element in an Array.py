6ae4b17d0504c5ed898510968598334137290714
graphMachine <wanglei430426@gmail.com>
2016-07-31 11:40:23 +0800

    Create Kth Largest Element in an Array.py

---
+++ Kth Largest Element in an Array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        import heapq
+        return heapq.nlargest(k,nums)[-1]
+       
+            
