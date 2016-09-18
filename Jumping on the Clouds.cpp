ba82e6cbdab5f65cc01a5b29aa94019d992ea75a
KiwiFlow <wanglei430426@gmail.com>
2016-09-18 11:39:03 +0800

    Create Jumping on the Clouds.cpp
    HR

---
+++ Jumping on the Clouds.cpp
@@ -0,0 +1,51 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> c(n);
+    for(int c_i = 0;c_i < n;c_i++){
+       cin >> c[c_i];
+    }
+    /*greedy*/
+    int jumps = 0;
+    int c_i = 0;
+    while(c_i!=n-1){
+        if(c_i+2<n){
+            if(c[c_i+2]==0){
+                c_i = c_i+2;
+            }else{
+                c_i = c_i+1;
+            }
+        }else{
+            c_i = c_i+1;
+        }
+        jumps++;
+    }
+    cout<<jumps;
+    return 0;
+}
