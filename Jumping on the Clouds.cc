7bc4230eb8c4e5b9fb6fd7ce0e1d58ffc1d7cff4
KiwiFlow <wanglei430426@gmail.com>
2016-09-19 13:53:26 +0800

    Rename Jumping on the Clouds.cpp to Jumping on the Clouds.cc
    HR

---
+++ Jumping on the Clouds.cc
@@ -0,0 +1,43 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> c(n);
+    for(int c_i = 0;c_i < n;c_i++){
+       cin >> c[c_i];
+    }
+    int pos = 0;
+    int e = 100;
+    do{
+        pos = (pos+k)%n;
+        e = e-1-c[pos]*2;
+    }while(pos!=0);
+    cout<<e;
+    return 0;
+}
