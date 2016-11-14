4c995b69db9fbd5c48b48dc933e50d52143e6e79
KiwiFlow <wanglei430426@gmail.com>
2016-11-14 18:25:50 +0800

    Update Balanced Brackets.cpp

--- Balanced Brackets.cpp
+++ Balanced Brackets.cpp
@@ -1,95 +1,73 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
-#include <unordered_map>
-
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<set>
+#include<cstdio>
+#include<map>
+#include<climits>
+#include<algorithm>
+#include<functional>
 using namespace std;
+const int NN = 50005;
+int arr[NN];
+const int MAX = 100000;
+int stTableMin[MAX][32];
+int stTableMax[MAX][32];
+int preLog2[MAX];
 
-void isBalanced(string &s){
-    if(!s.size()){
-        cout<<"NO"<<endl;
-        return;
-    }
-    stack<char> match;
-    for(int i=0;i<s.size();i++){
-        if(s[i]=='{' || s[i]=='[' || s[i]=='('){
-            match.push(s[i]);
-        }else{
-            if(!match.empty() && ((s[i]=='}'&&match.top()=='{') ||
-                  (s[i]==']'&&match.top()=='[') || (s[i]==')'&&match.top()=='('))){
-                match.pop();
-            }else{
-                cout<<"NO"<<endl;
-                return;
-            }
-        }
-    }
-    if(match.empty()){
-        cout<<"YES"<<endl;
-        return;
-    }else{
-        cout<<"NO"<<endl;
-        return;
-    }
-}
-
-/*bool isValid(string &s) {
-		if(s.empty())	return false;
-        stack<char> matchStack;
-		for(int i=0;i!=s.size();i++)
-		{
-			if(!matchStack.empty())
-			{
-				char frontChar=matchStack.top();
-				if(frontChar=='(' && s[i]==')'
-				  || frontChar=='[' && s[i]==']'
-				  || frontChar=='{' && s[i]=='}')
-				{
-					matchStack.pop();
-				}
-				else
-				{
-					matchStack.push(s[i]);
-				}
-			}
-			else
-			{
-				matchStack.push(s[i]);
-			}
+void preLog_prepare(int n){
+    preLog2[1] = 0;
+	for (int i = 2; i <= n; i++) {
+		preLog2[i] = preLog2[i - 1];
+		if ((1 << preLog2[i] + 1) == i) {
+			preLog2[i]++;
 		}
-		return matchStack.empty();
-    }*/
-
-int main(){
-    int t;
-    cin >> t;
-    for(int a0 = 0; a0 < t; a0++){
-        string s;
-        cin >> s;
-        isBalanced(s);
-        /*if(isValid(s)){
-            cout<<"YES"<<endl;
-        }else{
-            cout<<"NO"<<endl;
-        }*/
-    }
-    return 0;
+	}
 }
+
+void st_prepare(int n, int *arr,int stTable[][32], int(*f)(int, int)){
+	for (int i = n - 1; i >= 0; i--) { 
+		stTable[i][0] = arr[i];
+		for (int j = 1; (i + (1 << j) - 1) < n; j++) {
+			stTable[i][j] = f(stTable[i][j - 1], stTable[i + (1 << j - 1)][j - 1]);
+		}
+    }
+}
+
+int maxF(int a, int b) {
+	return max(a, b);
+}
+int minF(int a, int b) {
+	return min(a, b);
+}
+
+int query(int L, int R, int stTable[][32],int (*f)(int,int)){
+	int len = R - L + 1, k = preLog2[len];
+	return f(stTable[L][k], stTable[R - (1 << k) + 1][k]);
+}
+
+int main() {
+	int n, m; scanf("%d%d", &n, &m);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+    preLog_prepare(n);
+	st_prepare(n, arr, stTableMax, maxF);
+	st_prepare(n, arr, stTableMin, minF);
+	for (int i = 0; i < m; i++) {
+		int L, R;
+		scanf("%d%d", &L, &R);
+		L--;
+		R--;
+		printf("%d\n", query(L, R, stTableMax, maxF) - query(L, R,stTableMin, minF));
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
