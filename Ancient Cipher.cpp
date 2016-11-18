a9e754e67bc7291809aa6819ecfb7b63f443be88
KiwiFlow <wanglei430426@gmail.com>
2016-11-18 20:59:49 +0800

    Update Ancient Cipher.cpp
    #uva

--- Ancient Cipher.cpp
+++ Ancient Cipher.cpp
@@ -9,26 +9,28 @@
 #include<climits>
 #include<vector>
 #include<map>
+#include<algorithm>
 using namespace std;
 
 char a[105], b[105];
+int da[26], db[26];
 int main() {
-	freopen("d:\\in.txt", "r", stdin);
-	freopen("d:\\out.txt", "w", stdout);
-	while (scanf("%s", a) != EOF) {
-		scanf("%s", b);
-		map<char, char> da, db;
+	//freopen("d:\\in.txt", "r", stdin);
+	//freopen("d:\\out.txt", "w", stdout);
+	while (scanf("%s%s", a, b) == 2) {
+		memset(da, 0, sizeof(da));
+		memset(db, 0, sizeof(db));
 		for (int i = 0; i < strlen(a); i++) {
-			da[a[i]]++;
-			db[b[i]]++;
+			da[a[i]-'A']++;
+			db[b[i]-'A']++;
 		}
+		sort(da, da + 26);
+		sort(db, db + 26);
 		bool ans = true;
-		auto itb = db.begin();
-		for (auto & val : da) {
-			if (val.second != itb->second) {
+		for (int i = 0; i < 26; i++) {
+			if (da[i] != db[i]) {
 				ans = false;
 			}
-			itb++;
 		}
 		printf("%s\n", ans ? "YES" : "NO");
 	}
