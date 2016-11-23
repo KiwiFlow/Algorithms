528bcfb36a163c726fc18cf0f02053ab88b1b837
KiwiFlow <wanglei430426@gmail.com>
2016-11-23 20:20:48 +0800

    Create Path Sum III.cpp
    #LC #cool

---
+++ Path Sum III.cpp
@@ -0,0 +1,54 @@
+/*
+You are given a binary tree in which each node contains an integer value.
+
+Find the number of paths that sum to a given value.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+Example:
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+Return 3. The paths that sum to 8 are:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3. -3 -> 11
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int pathSum(TreeNode* root, int sum) {
+        int ans=0;
+        getSum(root,sum,ans);
+        return ans;
+    }
+    void getSum(TreeNode* root,int sum,int &ans){
+        if(!root) return;
+        ans += segSum(root,sum);
+        getSum(root->left,sum,ans);
+        getSum(root->right,sum,ans);
+    }
+    int segSum(TreeNode* root,int sum){
+        if(!root) return 0;
+        return (root->val==sum?1:0)+segSum(root->left,sum-root->val)+segSum(root->right,sum-root->val);
+    }
+};
