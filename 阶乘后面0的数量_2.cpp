80faf4daf9ea8fc0da229ca096ce99b39dac8bd3
KiwiFlow <wanglei430426@gmail.com>
2017-08-24 11:42:11 +0800

    Create 阶乘后面0的数量_2.cpp
    nod

---
+++ 阶乘后面0的数量_2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    // int a = 5;
+    // int res = 0;
+    // while(n>=a){
+    //     res += n/a;
+    //     a*=5;
+    // }
+    int res = 0;
+    while(n){
+        n /= 5;
+        res += n;
+    }
+    cout<<res;
+}
